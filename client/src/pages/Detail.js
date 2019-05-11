7476f57d1b35b4d545e32ce04e198694d3af7eee
Sally Beckwith <sallypbeckwith@gmail.com>
2019-05-10 21:25:16 -0400

    Can now enter new player, view their high score, or delete a player

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -5,13 +5,14 @@ import API from "../utils/API";
 
 class Detail extends Component {
   state = {
-    book: {}
+    user: {}
   };
-  // When this component mounts, grab the book with the _id of this.props.match.params.id
-  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
+  // When this component mounts, grab the user with the _id of this.props.match.params.id
+  // e.g. localhost:3000/users/599dcb67f0f16317844583fc
   componentDidMount() {
-    API.getBook(this.props.match.params.id)
-      .then(res => this.setState({ book: res.data }))
+    console.log('inside Detail: id = ' + this.props.match.params.id);
+    API.getUser(this.props.match.params.id)
+      .then(res => this.setState({ user: res.data }))
       .catch(err => console.log(err));
   }
 
@@ -22,24 +23,18 @@ class Detail extends Component {
           <Col size="md-12">
             <Jumbotron>
               <h1>
-                {this.state.book.title} by {this.state.book.author}
+                {this.state.user.firstName} {this.state.user.lastName}
+                </h1>
+                <h1>
+                High Score: {this.state.user.highScore}
               </h1>
             </Jumbotron>
           </Col>
         </Row>
-        <Row>
-          <Col size="md-10 md-offset-1">
-            <article>
-              <h1>Synopsis</h1>
-              <p>
-                {this.state.book.synopsis}
-              </p>
-            </article>
-          </Col>
-        </Row>
+
         <Row>
           <Col size="md-2">
-            <Link to="/">← Back to Authors</Link>
+            <Link to="/">← Back to Home</Link>
           </Col>
         </Row>
       </Container>
