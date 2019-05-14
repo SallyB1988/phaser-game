dc9d39072445c385936fe2057ad6667f51ae1562
Bryan Villatoro <bvillatoro@ymail.com>
2019-05-14 16:44:31 -0400

    added camera

--- client/src/components/scenes/Game.js
+++ client/src/components/scenes/Game.js
@@ -24,20 +24,23 @@ export default class Game extends Phaser.Scene {
     }
 
     create = () => {
-        this.mWorld = this.matter.world.setBounds(0, 0, 800, 600, 32, true, true, true, true);
+        this.mWorld = this.matter.world.setBounds(0, 0, 1600, 1200, 32, true, true, true, true);
         this.mWorld.on("collisionstart",(pair,bod1,bod2)=>{
             if(bod1.gameObject && bod1.gameObject && bod1.gameObject.name !== "player"){
             bod1.gameObject.destroy();
             bod2.gameObject.destroy();
-
             }
-
         })
+
+        this.add.image(0, 0, KEYS.IMAGES.Stars).setScale(4);
+        this.Ship = new Player(this.mWorld, 400, 200, KEYS.SPRITES.GreenShip)
+        this.cameras.main.startFollow(this.Ship)
+        .setZoom(.5);
         
 
-        this.add.image(0, 0, KEYS.IMAGES.Stars).setOrigin(0, 0).setScale(.8);
-        this.Ship = new Player(this.mWorld, 400, 200, KEYS.SPRITES.GreenShip)
+
         this.Bad = new Enemy(this.mWorld, 0, 0, KEYS.SPRITES.Enemy);
+
         new KeyboardV2(this, this.Ship);
       
         this.input.keyboard.on('keyup_P', (e) => {
@@ -46,9 +49,10 @@ export default class Game extends Phaser.Scene {
             .setDisplaySize(30, 30)
             .setIgnoreGravity(true)
             .setAngle(this.Ship.angle - 90)
-            .thrust(.02 + this.Ship.speed)
+            .thrust(.02 + this.Ship.body.speed/150)
             .setCollisionCategory(8)
             .setCollidesWith([1,4])
+            .setFrictionAir(0)
             .play("shoot");
         })
     }
