c8f4b6a014751dee83b1f2c24f7cc5f88542ce67
B-Villatoro <bvillatoro@ymail.com>
2019-05-16 18:42:16 -0400

    delete junk

--- client/src/components/scenes/Gameover.js
+++ client/src/components/scenes/Gameover.js
@@ -1,6 +1,4 @@
 import Phaser from "phaser";
-import DB from "../../controllers/usersController"
-
 export default class Gameover extends Phaser.Scene {
     constructor() {
         super({ key: "Gameover" })
