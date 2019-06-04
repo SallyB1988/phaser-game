63fd09c7463d9c4179b3e7ad48acec65c340d9b7
Sally Beckwith <sallypbeckwith@gmail.com>
2019-06-04 00:25:19 -0400

    fixed some styling issues

--- client/src/components/Instructions/Instructions.js
+++ client/src/components/Instructions/Instructions.js
@@ -1,74 +1,70 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import './Instructions.css';
 
 // PlayerCard renders an image
 function Instructions() {
   return (
-    <Row className="px-5 justify-content-center">
-      <Col>
-        <p className="my-2">
-          Have you been working hard and need a break to clear your mind? You've
+    <Row className="justify-content-center">
+      {/* <Col> */}
+        <p id="intro" className="mx-5">
+          Need a break to clear your mind? You've
           come to the right place! Take a few minutes and play an old-fashioned
           arcade style game.
         </p>
 
-        <h2 className="text-center py-2">Instructions</h2>
-        <hr />
-        <p>
-          Go to the login page and enter your name. You may also choose a theme
-          and a panic screen to display in case the boss walks in! Your high
+        <h3 className="text-center">Instructions</h3>
+        <p id="instruction-text" className="mx-5">
+          Enter your name on the Login page, select a theme
+          and a panic screen type.  Your high
           score will be recorded so you can come back later and try to beat it.
-        </p>
-        <p>
-          You can just click the play game tab and play as a guest, but your
-          score will not be recorded or ranked.
+          Or play as a guest, but your score will not be recorded or ranked.
         </p>
 
 
-        <Row className="py-2">
+        <Row>
         <Col md={12}>
           
-          <h2 className="text-center">Controls</h2>
-          <hr/>
+          <h3 className="text-center py-2">Controls</h3>
           </Col>
         <Col md={5} className="font-weight-bold text-right">
             Left/Right Arrows:
           </Col>
           <Col md={7} className="text-left">
-            Rotates the spaceship
+            Rotate the spaceship
           </Col>
           <Col md={5} className="font-weight-bold text-right">
             Up/Down Arrows:
           </Col>
           <Col md={7} className="text-left">
-            Moves ship forward/backward
+            Move ship forward/backward
           </Col>
           <Col md={5} className="font-weight-bold text-right">
             Space Bar:
           </Col>
           <Col md={7} className="text-left">
-            Shoots missiles
+            Shoot missiles
           </Col>
           <Col md={5} className="font-weight-bold text-right">
             Enter:
           </Col>
           <Col md={7} className="text-left">
-            Pauses game (hit Enter to resume)
+            Pause game (hit Enter to resume)
           </Col>
           <Col md={5} className="font-weight-bold text-right">
             M:
           </Col>
           <Col md={7} className="text-left">
-            Toggles sounds
+            Toggle sounds
           </Col>
           <Col md={5} className="font-weight-bold text-right">
             P:
           </Col>
           <Col md={7} className="text-left">
-          Displays Panic screen image. Hit escape to return to game.
+          Display Panic screen image. Hit escape to return to game.
           </Col>
         </Row>
-      </Col>
+      {/* </Col> */}
     </Row>
   );
 }
--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { IndexLinkContainer } from "react-router-dom";
 
 import { Navbar, Nav, Button } from "react-bootstrap";
 import { IndexLinkContainer } from "react-router-bootstrap";
@@ -9,11 +8,11 @@ function Navigation(props) {
   const { routes } = props;
 
   return (
-    <Navbar bg="transparent" expand="lg" className="pt-1 m-0">
+    <Navbar bg="transparent" className="pt-1 m-0">
       <Nav>
         {routes.map(r => {
           return (
-            <IndexLinkContainer className="nav-style" key={r.path} to={r.path}>
+            <IndexLinkContainer  key={r.path} to={r.path}>
               <Button className="button-style">{r.name}</Button>
             </IndexLinkContainer>
           );
